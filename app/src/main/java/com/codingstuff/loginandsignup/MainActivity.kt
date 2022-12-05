7291bb646b4d9d0d46b665a8b719a07dbe7c837c
muhammedsafa <msafatur@outlook.com>
2022-12-05 14:52:09 +0300

    nav_bar

--- app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
+++ app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
@@ -2,10 +2,14 @@ package com.codingstuff.loginandsignup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codingstuff.loginandsignup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
