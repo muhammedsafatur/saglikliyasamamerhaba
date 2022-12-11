f5e19d80dbad1960dea60784ff5260f1c0f0217a
muhammedsafa <msafatur@outlook.com>
2022-12-12 02:05:02 +0300

    nav_bar_compt

--- app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
+++ app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.codingstuff.loginandsignup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.codingstuff.loginandsignup.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -11,5 +13,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
         setContentView(binding.root)
+        fun replaceFragment (fragment: Fragment ){
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.frame_layout,fragment)
+            fragmentTransaction.commit()
+        }
+
+        replaceFragment(anasayfa())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+             R.id.Anasayfa -> replaceFragment(anasayfa())
+                R.id.bilgi -> replaceFragment(bilgi())
+                R.id.kisisel_veri -> replaceFragment(kisisel_veri())
+                R.id.egzersiz -> replaceFragment(egzersiz())
+            }
+
+            true
+        }
+
     }
 }
