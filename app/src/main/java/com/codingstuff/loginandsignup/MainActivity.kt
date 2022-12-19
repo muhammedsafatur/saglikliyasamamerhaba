5fd3c553b312ec9c918b761d8f6c5439d3f075d8
muhammedsafa <msafatur@outlook.com>
2022-12-19 14:03:04 +0300

    final nav bar !!!

--- app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
+++ app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
@@ -2,38 +2,24 @@ package com.codingstuff.loginandsignup
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.codingstuff.loginandsignup.databinding.ActivityMainBinding
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        setContentView(binding.root)
-        fun replaceFragment (fragment: Fragment ){
-            val fragmentManager = supportFragmentManager
-            val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frame_layout,fragment)
-            fragmentTransaction.commit()
-        }
 
-        replaceFragment(anasayfa())
+val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+ navController = navHostFragment.navController
+val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-
-            when(it.itemId){
-
-             R.id.Anasayfa -> replaceFragment(anasayfa())
-                R.id.bilgi -> replaceFragment(bilgi())
-                R.id.kisisel_veri -> replaceFragment(kisisel_veri())
-                R.id.egzersiz -> replaceFragment(egzersiz())
-            }
-
-            true
-        }
-
+       setupWithNavController(bottomNavigationView, navController)
     }
+
+
 }
